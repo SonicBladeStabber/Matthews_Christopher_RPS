b15352fd7716c5282313f85baf99368ea70dcfdb
Christopher Matthews <chrisaaronm@gmail.com>
2021-10-06 17:37:24 -0400

    added pytyhon file

---
+++ main.py
@@ -0,0 +1,12 @@
+name = "Christopher"
+eyeColour = "blue-green mix"
+heightInFeet = "5'6"
+hairColour = "Brown w/ golden highlights"
+
+print("========================")
+print("My name is " + name + " and i have " + eyeColour + " eyes")
+
+print("I'm about " + heightInFeet + " tall and have " + hairColour + " hair.")
+
+print(" I sound younger than I actually am.")
+print("========================")
