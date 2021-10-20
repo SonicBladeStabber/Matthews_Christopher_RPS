c0e3e59f1cb6ba112d4c0e4d2d2dfdb62980a563
Christopher Matthews <chrisaaronm@gmail.com>
2021-10-20 17:27:01 -0400

    Added More lines of code to main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ time.sleep(0.5)
 
 CPU = weapon[randint(0, 2)]
 
+playerLives = 5
+computerLives = 5
+
 print("player chose: " + player)
 time.sleep(0.5)
 print("computer chose: " + CPU)
@@ -22,21 +25,30 @@ if (CPU == player):
 elif (player == "rock"):
 	if (CPU == "paper"):
 		print("you loose!")
+		playerLives = playerLives - 1
+
 	else:
 		print("you win!")
+		computerLives = computerLives - 1
 
 
 elif (player == "paper"):
 	if (CPU == "scissors"):
 		print("you loose!")
+		playerLives = playerLives - 1
 	else:
 		print("you win!")
+		computerLives = computerLives - 1
 
 
 elif (player == "scissors"):
 	if (CPU == "rock"):
 		print("you loose!")
+		playerLives = playerLives - 1
 	else:
 		print("you win!")
+		computerLives = computerLives - 1
 
+print("player lives: " + str(playerLives))
+print("computer lives: " + str(computerLives))
 time.sleep(3)
