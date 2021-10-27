70774a71fffcdc48c40c19948a50a7c4bfe630a2
Christopher Matthews <chrisaaronm@gmail.com>
2021-10-27 17:32:30 -0400

    Finalized files

---
+++ gameComponents/gameVars.py
@@ -0,0 +1,12 @@
+import time
+
+weapon = ["rock", "paper", "scissors"]
+#----------------------------------------
+# player defines which choise the player chooses via input
+
+player= False
+
+time.sleep(0.5)
+
+playerLives = 5
+computerLives = 5
--- main.py
+++ main.py
@@ -1,54 +1,63 @@
 import time
+from gameComponents import WinLose, gameVars
 from random import randint
 #----------------------------------------
-weapon = ["rock", "paper", "scissors"]
-#----------------------------------------
-# player defines which choise the player chooses via input
-player = input("Choose your weapon: rock, paper or scissors:")
-
-time.sleep(0.5)
-
-CPU = weapon[randint(0, 2)]
-
-playerLives = 5
-computerLives = 5
-
-print("player chose: " + player)
-time.sleep(0.5)
-print("computer chose: " + CPU)
-
-time.sleep(1.5)
-
-if (CPU == player):
-	print("tie! Try again")
-
-elif (player == "rock"):
-	if (CPU == "paper"):
-		print("you loose!")
-		playerLives = playerLives - 1
-
-	else:
-		print("you win!")
-		computerLives = computerLives - 1
 
 
-elif (player == "paper"):
-	if (CPU == "scissors"):
-		print("you loose!")
-		playerLives = playerLives - 1
-	else:
-		print("you win!")
-		computerLives = computerLives - 1
+# set up our game loop so that we can keep playing and not exit
+while gameVars.player is False:
+	gameVars.player = input("Choose your weapon: rock, paper or scissors: ")
+	CPU = gameVars.weapon[randint(0, 2)]
+
+	print("player chose: " + gameVars.player)
+	time.sleep(0.3)
+	print("computer chose: " + CPU)
+
+	time.sleep(1)
+
+	if (CPU == gameVars.player):
+		print("tie! Try again")
+
+	elif (gameVars.player == "rock"):
+		if (CPU == "paper"):
+			print("you loose!")
+			gameVars.playerLives = gameVars.playerLives - 1
+
+		else:
+			print("you win!")
+			gameVars.computerLives = gameVars.computerLives - 1
 
 
-elif (player == "scissors"):
-	if (CPU == "rock"):
-		print("you loose!")
-		playerLives = playerLives - 1
-	else:
-		print("you win!")
-		computerLives = computerLives - 1
+	elif (gameVars.player == "paper"):
+		if (CPU == "scissors"):
+			print("you loose!")
+			gameVars.playerLives = gameVars.playerLives - 1
+		else:
+			print("you win!")
+			gameVars.computerLives = gameVars.computerLives - 1
 
-print("player lives: " + str(playerLives))
-print("computer lives: " + str(computerLives))
-time.sleep(3)
+
+	elif (gameVars.player == "scissors"):
+		if (CPU == "rock"):
+			print("you loose!")
+			gameVars.playerLives = gameVars.playerLives - 1
+		else:
+			print("you win!")
+			gameVars.computerLives = gameVars.computerLives - 1
+
+	print("player lives: " + str(gameVars.playerLives))
+	print("computer lives: " + str(gameVars.computerLives))
+
+
+	if gameVars.playerLives == 0:
+		WinLose.WinOrLose("lost")
+
+	elif gameVars.computerLives == 0:
+		WinLose.WinOrLose("won")
+		gameVars.player = False
+
+	gameVars.player = False
+
+	time.sleep(1)
+
+	print("------------------------------------------------------------------")
